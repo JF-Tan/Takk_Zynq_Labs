796f2aff6abbe89a65aae83823bd9ef974f08ab2
Takk-Tanee <95535693+Takk-Tanee@users.noreply.github.com>
2022-01-13 21:52:22 +0800

    Add files via upload

---
+++ Lab_01_PS/project.sdk/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ *
+ *  Created on: 2021Äê12ÔÂ31ÈÕ
+ *      Author: takk_Zynq_labs Lab_01 PS
+ */
+
+
+#include <stdio.h>
+#include <sleep.h>
+int main(){
+
+	while(1){
+		sleep(1);
+		printf("Takk_Zynq_Labs nice! \n\r");//from Zynq board uart
+	}
+
+
+}
+
